2c371d16829c281692fd04187c2cef8f83d3a58f
RAGHAV AGGARWAL <look4raghavworld@gmail.com>
2023-03-30 01:08:08 +0530

    -

---
+++ input [net_type] [range] list_of_name;.v
@@ -0,0 +1,4 @@
+input [net_type] [range] list_of_name;
+inout [net_type] [range] list_of_name;
+output [net_type] [range] list_of_name;
+output [var_type] [range] list_of_name;
---
+++ module my_design (input wire clk,.v
@@ -0,0 +1,7 @@
+module my_design (input wire clk,
+				  input en,
+				  input rw,
+				  input [15:0] data,
+				  output int);
+
+endmodule
