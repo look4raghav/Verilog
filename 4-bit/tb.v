1cc6135050c640e86c1c360755491021c922fa24
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-04-28 22:26:41 +0530

    Update tb.v

--- 4-bit/tb.v
+++ 4-bit/tb.v
@@ -2,6 +2,7 @@ module test;
   reg up_down, clk, reset;
   wire [3:0]q;
   up_down_counter test(q,up_down,clk,reset);
+  
   initial begin
     $dumpfile("test.vcd");
     $dumpvars(1);
@@ -10,6 +11,8 @@ module test;
     reset=0;
     #200 $finish;
   end
+  
   always #80 up_down=~up_down;
   always #8 clk=~clk;
+  
 endmodule
