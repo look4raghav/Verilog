8190c6d289214d175c16538ada01181314938565
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-05-03 22:06:44 +0530

    Update 4-bit-FA.v

--- 4-bit-FA.v
+++ 4-bit-FA.v
@@ -7,8 +7,8 @@ module full_adder_4bit_st(s, cout, a, b, cin)
   wire n1, n2, n3;
   
   full_adder_bh fa1(s[0], n1, a[0], b[0], cin);
-  full_adder_bh fa2(s[1], n2, a[1], b[1], cin);
-  full_adder_bh fa3(s[2], n3, a[2], b[2], cin);
-  full_adder_bh fa4(s[3], cout, a[3], b[3], cin);
+  full_adder_bh fa2(s[1], n2, a[1], b[1], n1);
+  full_adder_bh fa3(s[2], n3, a[2], b[2], n2);
+  full_adder_bh fa4(s[3], cout, a[3], b[3], n3);
   
 endmodule
