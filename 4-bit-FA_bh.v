65f609acf8ffdadcb18aff27a34cc6ed6b9afe7e
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-05-03 22:24:21 +0530

    Create 4-bit-FA_bh.v

---
+++ 4-bit-FA_bh.v
@@ -0,0 +1,10 @@
+module full_adder_4bit_bh(s, cout, a, b, cin)
+  input [3:0] a, b;
+  input cin;
+  output reg[3:0]s;
+  output reg cout;
+  
+  always@(a,b,cin)
+  {cout,s} = a + b + cin;
+  
+endmodule
