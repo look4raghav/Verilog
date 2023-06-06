b80a2a8747082857cea50ea3b561c7764cc8a46c
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-06 20:48:39 +0530

    Create dffbbb.v

---
+++ dffbbb.v
@@ -0,0 +1,20 @@
+
+module dffb(q,d,clk,rst,set);
+
+	input d,clk,rst,set;
+	output reg q;
+
+	always@(posedge clk)
+		if(!rst)
+			q <= 1'b0;
+		else if(set)
+			q <= 1'b1;
+		else
+			q <= d;
+
+endmodule
+
+		else
+			q <= d;
+
+endmodule
