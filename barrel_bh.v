11884a1fbc2d4f6adbe44b5001dacd4ab0c1e0e2
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-06 12:09:08 +0530

    Create barrel_bh.v

---
+++ barrel_bh.v
@@ -0,0 +1,13 @@
+module barrel_bh(Out, In, Lr, n);
+  input[7:0]In;
+  input[2:0]n;
+  input Lr;
+  output reg [7:0]Out;
+  
+  always@(*) begin
+    if(Lr)
+      Out = In << n;
+    else
+      Out = In >> n;
+  end
+endmodule
