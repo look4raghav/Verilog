5e3d9aa530a778412c6c94e8ba3aadb8b91b829a
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-08 08:26:34 +0530

    Update single_port_ram.v

--- single_port_ram.v
+++ single_port_ram.v
@@ -1,7 +1,7 @@
 module ram_single_port1(
-  output reg [7:0]q;
-  input [7:0] data;
-  input [5:0] read_addr, write_addr;
+  output reg [7:0]q,
+  input [7:0] data,
+  input [5:0] read_addr, write_addr,
   input we, clk
 );
   reg[7:0] ram[63:0];
