67fa4c5d52b177589ca9e2d05ce4a3c6eb3130a6
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-06-08 09:47:05 +0530

    Create ram_single_port.v

---
+++ ram_single_port.v
@@ -0,0 +1,15 @@
+module ram_single_port2(
+  output reg [7:0]q,
+  input [7:0] data,
+  input [5:0] read_addr, write_addr,
+  input we, clk
+);
+  reg [7:0] ram [63:0];
+  
+  always@(posedge clk)
+    begin
+      if(we)
+        ram[write_addr] = data;
+      q = ram[read_addr];
+    end
+endmodule
